0198e387823c9a21497f8db0468b731f86a0e3e0
nitinbhatti1907 <nitinbhatti1907@.com>
2026-03-29 18:59:26 -0400

    deploy fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,9 +16,9 @@ app = FastAPI(title="Canadian Superstore Dashboard API", version="1.0.0")
 # CORS for local dev
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
-    allow_methods=["*"] ,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
